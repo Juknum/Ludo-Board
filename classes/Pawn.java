bbf88f134535efe45ff5e9ccc6fad452d35c28d0
Noé Echard <>
2021-04-02 09:52:47 +0000

    Update

---
+++ classes/Pawn.java
@@ -0,0 +1,22 @@
+package fr.constant-echard.ludo;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Pawn{
+
+  int x, y;
+  int current;
+  int height, width;
+
+  puclic Pawn(int h, int w){
+    current = -1;
+    x = -1;
+    y = -1;
+    height = h;
+    width = w;
+  }
+
+
+}
