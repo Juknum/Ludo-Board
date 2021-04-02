f05882eba8783d86d78aa36a4797ae911192af9a
Noé Echard <>
2021-04-02 10:50:28 +0000

    Update

---
+++ src/GameScreen.java
@@ -0,0 +1,15 @@
+package fr.constant-echard.ludo;
+
+import javax.swing.JFrame;
+
+public class GameScreen{
+  public static void main(String[] args){
+    JRframe jframe = new JFrame();
+    jframe.setBounds(10, 10, 1000, 600);
+    jframe.setTitle("LUDO");
+    jframe.setResizable(false);
+    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //TODO parametres avec gamemoves
+  }
+}
